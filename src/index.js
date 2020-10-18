3b66c309999ecb95c932bcd3dbc79085df5bed58
Lefevre-Alexandre <jean.lefevre.alexandre@gmail.com>
2020-10-19 00:01:19 +0400

    ajout des composants acceuil, contact, header + définitions des routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
